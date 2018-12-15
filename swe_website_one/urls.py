3d3d7cad56e3930fe80f0edd99d6021bba69c810
tonoy30 <tttono786@gmail.com>
2018-12-16 01:29:12 +0600

    commit-happened

--- swe_website_one/urls.py
+++ swe_website_one/urls.py
@@ -1,6 +1,9 @@
 from . import views
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path("bulma/", views.bulma, name='bulma'),
+    path('registration/', views.registration, name='registration'),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
--- swe_website_one/views.py
+++ swe_website_one/views.py
@@ -1,7 +1,22 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 
 
 def home(request):
+    if request.method == "POST":
+        reg_no = request.POST['reg_no']
+        email = request.POST['email']
+        print("Reg No:", reg_no)
+        print("Email: ", list(email))
+        return render(request, 'bulma.html',{})
     return render(request, 'home.html', {})
+
+
+def bulma(request):
+    return render(request, 'bulma.html', {})
+
+
+def registration(request):
+    return render(request, 'registration.html', {})
