0d7865223e9c40121ad4150478d3f30a025d1215
Rafiul Islam <avoidcloud@gmail.com>
2019-02-18 22:49:15 +0600

    pagination view in feeds

--- static/post/script.js
+++ static/post/script.js
@@ -1,24 +1,13 @@
-function show() {
-	document.querySelector(".pop-window").style.display = 'flex';
-}
 
-function hide() {
-	document.querySelector(".pop-window").style.display = 'none';
-}
 window.onscroll = function() { scrollAction() };
 
 function scrollAction() {
     if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
         document.getElementsByClassName('tab-section')[0].style.position = 'fixed';
-        document.getElementsByClassName('tab-section')[0].style.marginTop = '-80px'
+        document.getElementsByClassName('tab-section')[0].style.marginTop = '-40px';
     }
     else{
         document.getElementsByClassName('tab-section')[0].style.position = 'relative';
         document.getElementsByClassName('tab-section')[0].style.marginTop = '80px'
     }
-}
-
-
-function showFull() {
-    
 }
